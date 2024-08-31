cfffce77f12bf847af07d5ddc9f936f84e2afa5b
jefferson silva <jefferson_1996Th@hotmail.com>
2024-08-31 17:45:08 -0300

    spring start

---
+++ spring/src/main/java/com/br_jeff/spring/Application.java
@@ -0,0 +1,13 @@
+package com.br_jeff.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
